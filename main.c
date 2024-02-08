d98689fdb3d78c1d7d3e6df92798e65efa8a27b7
Hayron <115109475+Hayron13@users.noreply.github.com>
2024-02-08 11:00:29 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+//INT - Valores Inteiros (4 bits)
+// LONG INT
+//SSHORT INT
+
+
+
+
+
+    void hello(){
+        printf("ze da manga\n");
+    }
+
+    int soma_dois_numeros( int num1, int num2){
+        return num1 + num2;
+    }
+
+
+
+    int main(){
+        hello();
+        int soma;
+        soma = soma_dois_numeros(2,3);
+
+        return 0;
+    }
